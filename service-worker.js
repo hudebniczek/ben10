a51e2ee6374948c34e6181883a1ff9ea53617c90
hudebniczek <radek.hudeczek@gmail.com>
2025-07-28 20:55:57 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,21 @@
 
-const CACHE_NAME = 'qr-map-app-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon.png',
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('qr-app').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon.png',
+        './libs/html5-qrcode.min.js'
+      ]);
+    })
   );
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
   );
 });
